8befb5dc5a37a780d27387e9872dd01784d46a32
Alec <hello@itsalec.co.uk>
2020-03-30 08:12:30 +0100

    Further class match attempt

--- src/Http/Resources/Routes/RouteResource.php
+++ src/Http/Resources/Routes/RouteResource.php
@@ -30,6 +30,25 @@ class RouteResource extends AbstractResource
                 if (class_exists($resource)) {
                     return new $resource($this->element);
                 }
+
+                // Try and guess relative to the actual class
+                $classSegments = explode('\\', get_class($this->element));
+
+                foreach ($classSegments as $index => $segment) {
+                    if ($segment == 'Models' || $segment == $class) {
+                        unset($classSegments[$index]);
+                    }
+                }
+
+                array_push($classSegments, 'Http');
+                array_push($classSegments, 'Resources');
+                array_push($classSegments, $class . 'Resource');
+
+                $resourceClass = implode('\\', $classSegments);
+
+                if (class_exists($resourceClass)) {
+                    return new $resourceClass($this->element);
+                }
             })],
         ];
     }
